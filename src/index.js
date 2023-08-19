6008d27e2bc326f879a18710aca84f128fc20ee1
FROSTYLAN <castillo089frosty@gmail.com>
2023-08-18 20:32:43 -0500

    Creando tu primer HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App/index.js";
+import "./index.css";
+
+function App(props) {
+  return (
+    <h1>
+      ¡{props.saludo}, {props.nombre}!
+    </h1>
+  );
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrapperComponent</p>
+        </React.Fragment>
+      );
+    }
+  };
+}
+
+const AppWithSaludo = withSaludo(App)("Ey");
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <AppWithSaludo nombre="Charles" />,
+  // <App saludo="Buenas" nombre="Nath" />,
+  document.getElementById("root")
 );
